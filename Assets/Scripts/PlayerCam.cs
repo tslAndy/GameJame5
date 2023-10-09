d087b98b687631e497bc9eb409a4fc7b66fadcb4
asorokin111 <Artem.Sorokin@vuoksi.fi>
2023-10-09 13:35:12 +0300

    Working player camera

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    [SerializeField]
+    private GameObject _cam;
+
+    private float _xRotation;
+    private float _yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX * 100;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY * 100;
+        _yRotation += mouseX;
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        _cam.transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
+    }
+}
