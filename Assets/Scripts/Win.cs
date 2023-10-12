fe808610f50e5899653a7927edbfbecb6aad4781
asorokin111 <Artem.Sorokin@vuoksi.fi>
2023-10-12 13:27:14 +0300

    Fixed win script

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class Win : MonoBehaviour
 {
     [SerializeField]
-    private CardCollector _collectedCards;
+    private ItemManager _collectedCards;
     [SerializeField]
     private GameObject _winScreen;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Exit"))
         {
-            _collectedCards.ShowExitStatus();
+            //_collectedCards.ShowExitStatus();
             if (_collectedCards.allCardsCollected)
             {
                 _winScreen.SetActive(true);
